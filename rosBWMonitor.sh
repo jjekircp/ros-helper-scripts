5deb5490b4d4f4e0a0c883c1221e97e4a2e61b5d
ibaranov <ibaranov@ROS.(none)>
2014-12-22 15:20:55 -0500

    first working version of BWMonitor

--- rosBWMonitor.sh
+++ rosBWMonitor.sh
@@ -2,9 +2,9 @@
 
 mapfile -t lines < <(rostopic list)
 for (( i=0; i<${#lines[@]}; i++ )); do 
-    echo ${lines[i]}
+    echo -n ${lines[i]} ", "
     read VAL1 <<< $(timeout 1s rostopic bw ${lines[i]} | awk '/average:/ { print $0}')
-    echo ${VAL1}
+    echo -n ${VAL1} ", "
     read VAL1 <<< $(timeout 1s rostopic hz ${lines[i]} | awk '/rate:/ { print $0}')
     echo ${VAL1}
 
