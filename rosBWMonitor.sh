584a038ca3bf1fe311ed0ace6b743bf1eaf10d03
User <user@learn.(none)>
2015-01-08 09:00:51 -0800

    made system sensitive to where message is published/subscribed

--- rosBWMonitor.sh
+++ rosBWMonitor.sh
@@ -1,11 +1,26 @@
 #! /bin/bash
 
 mapfile -t lines < <(rostopic list)
+now=$(date)
+host=$(hostname)
+echo $now
+echo "Topic, Bandwidth, Rate, Published Locally?, Subscribed Locally?"
 for (( i=0; i<${#lines[@]}; i++ )); do 
     echo -n ${lines[i]} ", "
     read VAL1 <<< $(timeout 1s rostopic bw ${lines[i]} | awk '/average:/ { print $0}')
     echo -n ${VAL1} ", "
     read VAL1 <<< $(timeout 1s rostopic hz ${lines[i]} | awk '/rate:/ { print $0}')
-    echo ${VAL1}
-
+    echo -n ${VAL1} ", "
+    read VAL1 <<< $(rostopic info ${lines[i]})
+    read VAL1 <<< $(rostopic info ${lines[i]})
+    VAL1=${VAL1//'*'/-} # Bodge to prevent * from throwing everything
+    sub=${VAL1##*'Subscribers:'}
+    pub=${VAL1##*'Publishers:'}
+    pub=${pub%%'Subscribers:'*}
+    if [[ $pub = *$host* ]]; then
+        echo -n "yes, "
+    fi
+    if [[ $sub = *$host* ]]; then
+        echo "yes"
+    fi
 done
